8b30b248dc8124064941e60773cc95718e7375b9
alexolomeo <alexolomeo@gmail.com>
2026-04-20 16:36:05 -0400

    validator history simple

--- labs/selinux-opsmode/simulate.sh
+++ labs/selinux-opsmode/simulate.sh
@@ -6,10 +6,12 @@ printf "%-5s Checking Log system......................................success\n"
 sleep 2
 printf "%-5s Ensuring rsyslog is installed on servera.................success\n"
 
-ansible-playbook -i /usr/local/rh134/labs/server.lab  /usr/local/rh134/labs/selinux-opsmode.yml -e "failover_mode='simulate"
+ansible-playbook -i /usr/local/rh134/labs/server.lab  /usr/local/rh134/labs/selinux-opsmode.yml -e "failover_mode='simulate'"
 #> /tmp/selinux-opsmode.log  2>&1
 
 
 sleep 2
 printf "%-5s Ensuring log files are not present on servera............success\n"
+
+cat /tmp/selinux-opsmode/simulate.log
 
