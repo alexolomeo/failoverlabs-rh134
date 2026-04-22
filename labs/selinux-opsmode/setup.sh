933390e430c7d55aedbcd0b1ec0f2327e8341457
alexolomeo <alexolomeo@gmail.com>
2026-04-22 18:15:50 -0400

    fix validator expiration lab

--- labs/selinux-opsmode/setup.sh
+++ labs/selinux-opsmode/setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+LAB_NAME=$1
+
 printf "%-5s Configurando entorno del lab logs-syslog...\n"
 
 sleep 2
@@ -7,7 +10,8 @@ printf "%-5s Checking Log system......................................success\n"
 sleep 2
 printf "%-5s Ensuring rsyslog is installed on servera.................success\n"
 
-ansible-playbook -i /usr/local/rh134/labs/server.lab  /usr/local/rh134/labs/selinux-opsmode.yml -e "failover_mode='start'" > /tmp/selinux-opsmode.log  2>&1
+ansible-playbook -i /usr/local/rh134/labs/server.lab  /usr/local/rh134/labs/selinux-opsmode.yml -e "failover_mode='start'" 
+#> /tmp/selinux-opsmode.log  2>&1
 
 
 #ansible-playbook -i /usr/local/rh134/labs/server.lab  /usr/local/rh134/labs/selinux-opsmode.yml -e "failover_mode='start'"
