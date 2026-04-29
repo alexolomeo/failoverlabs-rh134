27930b74a42acc94d2f6080ccf2b8b304f8797c6
alexolomeo <alexolomeo@gmail.com>
2026-04-29 06:16:25 -0400

    Fix installation methtod

--- labs/init/configurate.sh
+++ labs/init/configurate.sh
@@ -2,9 +2,9 @@
 
 LAB_NAME=$1
 
-rm -rf /tmp/$LAB_NAME.log
+#rm -rf /tmp/$LAB_NAME.log
 
-printf "%- Configurando dependencias ...\n"
+printf " Configurando dependencias ...\n"
 
 sleep 2
 printf "%-5s Checking Workstation.....................................success\n"
@@ -14,6 +14,9 @@ printf "%-5s Checking servera.........................................success\n"
 
 sleep 2
 printf "%-5s Checking serverb.........................................success\n"
+
+#ansible-playbook -i /usr/local/rh134/labs/server.lab  /usr/local/rh134/labs/$LAB_NAME.yml -e "failover_mode='configurate'" 
+#  >  /tmp/$LAB_NAME.log  2>&1
 
 ansible-playbook -i /usr/local/rh134/labs/server.lab  /usr/local/rh134/labs/$LAB_NAME.yml -e "failover_mode='configurate'"  >  /tmp/$LAB_NAME.log  2>&1
 
