04888de7d05d4ccfd3949e805e4acf1f1e6580c0
alexolomeo <alexolomeo@gmail.com>
2026-04-30 14:30:02 -0400

    fix init tas to create configuration file and validate

--- labs/init/configure.sh
+++ labs/init/configure.sh
@@ -2,9 +2,7 @@
 
 LAB_NAME=$1
 
-#rm -rf /tmp/$LAB_NAME.log
-
-printf " Configurando dependencias ...\n"
+rm -rf /tmp/$LAB_NAME.log
 
 sleep 2
 printf "%-5s Checking Workstation.....................................success\n"
@@ -15,13 +13,13 @@ printf "%-5s Checking servera.........................................success\n"
 sleep 2
 printf "%-5s Checking serverb.........................................success\n"
 
-#ansible-playbook -i /usr/local/rh134/labs/server.lab  /usr/local/rh134/labs/$LAB_NAME.yml -e "failover_mode='configurate'" 
+#ansible-playbook -i /usr/local/rh134/labs/server.lab  /usr/local/rh134/labs/$LAB_NAME.yml -e "failover_mode='configure'" 
 #  >  /tmp/$LAB_NAME.log  2>&1
 
-ansible-playbook -i /usr/local/rh134/labs/server.lab  /usr/local/rh134/labs/$LAB_NAME.yml -e "failover_mode='configurate'"  >  /tmp/$LAB_NAME.log  2>&1
+ansible-playbook -i /usr/local/rh134/labs/server.lab  /usr/local/rh134/labs/$LAB_NAME.yml -e "failover_mode='configure'"  >  /tmp/$LAB_NAME.log  2>&1
 
 sleep 2
 printf "%-5s Activate Rsync............................................success\n"
 printf "....................................................................................................\n"
 
-cat /tmp/$LAB_NAME.log | tail -3
+cat /tmp/$LAB_NAME.log | tail -4
