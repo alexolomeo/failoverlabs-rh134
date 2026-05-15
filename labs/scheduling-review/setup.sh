3c39dd3df505dd9aee3c31d276efa840fd0ffa55
alexolomeo <alexolomeo@gmail.com>
2026-05-15 12:04:12 -0400

    add new validation for scheduling-review

--- labs/scheduling-review/setup.sh
+++ labs/scheduling-review/setup.sh
@@ -4,21 +4,25 @@ LAB_NAME=$1
 
 printf "%-5s Preparando el entorno del lab...\n"
 
-ansible-playbook -i /usr/local/rh134/labs/server.lab  /usr/local/rh134/labs/scheduling-review.yml -e "failover_mode='start'"  >  /tmp/$LAB_NAME.log  2>&1
+ansible-playbook -i /usr/local/rh134/labs/server.lab  /usr/local/rh134/labs/scheduling-review.yml -e "failover_mode='start'"  >  /tmp/scheduling-review.log  2>&1
 
 sleep 2
-printf "%-5s Checking  System.........................................success\n"
+printf "%-5s Checking  System...................................................success\n"
 
 sleep 2
 printf "%-5s Ensuring scheduling-review is installed on servera.................success\n"
 
 
+sleep 2
+printf "%-5s Ensuring and clean all scheduling task on servera..................success\n"
+
 
 sleep 2
-printf "%-5s Ensuring init config on servera.........................success\n"
+printf "%-5s Ensuring init config on servera....................................success\n"
 printf "....................................................................................................\n"
 
-cat /tmp/$LAB_NAME.log | tail -3
 
+
+cat /tmp/$LAB_NAME.log | tail -3
 
 
--- labs/scheduling-review/simulate.sh
+++ labs/scheduling-review/simulate.sh
@@ -2,14 +2,18 @@
 
 LAB_NAME=$1
 
-printf "%-5s Configurando entorno y procesando las validaciones...\n"
+printf "%-5s Configurando entorno y procesando las validaciones................\n"
 sleep 2
-printf "%-5s Checking system..........................................success\n"
+printf "%-5s Checking system...................................................success\n"
 
 sleep 2
 printf "%-5s Ensuring scheduling tasku is installed on servera.................success\n"
 
 ansible-playbook -i /usr/local/rh134/labs/server.lab  /usr/local/rh134/labs/scheduling-review.yml -e "failover_mode='simulate'" > /tmp/$LAB_NAME.log  2>&1
 
+printf "%-5s \n"
 
 cat /tmp/$LAB_NAME.log | tail -3
+
+printf "%-5s \n"
+
