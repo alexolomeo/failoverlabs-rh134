be3640e8b0c224ac751d9af9cf9338c2c1d70829
alexolomeo <alexolomeo@gmail.com>
2026-06-07 13:07:12 -0400

    add features  permisible mode strict validata and grade

--- labs/scheduling-at/simulate.sh
+++ labs/scheduling-at/simulate.sh
@@ -50,7 +50,9 @@ if [ -f "/tmp/$LAB_NAME.log" ]; then
 	sudo rm -rf /tmp/$LAB_NAME.log
 fi
 
-sudo ansible-playbook -i /usr/local/rh134/labs/server.lab  /usr/local/rh134/labs/$LAB_NAME.yml -e "failover_mode='simulate'" >  /tmp/$LAB_NAME.log  2>&1
+sudo ansible-playbook -i /usr/local/rh134/labs/server.lab  /usr/local/rh134/labs/$LAB_NAME.yml -e "failover_mode='simulate'" 
+
+#>  /tmp/$LAB_NAME.log  2>&1
 
 echo "Starting lab."
 echo ""
